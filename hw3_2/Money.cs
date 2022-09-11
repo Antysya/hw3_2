679626e5e6d12c66774dc3eb458c2ca94b3c5c6f
Пользователь <@DESKTOP-6V93RUO>
2022-09-11 15:42:01 +0300

    Добавьте файлы проекта.

---
+++ hw3_2/Money.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw3_2
+{
+    internal class Money
+    {
+        public int Rub { get; set; }
+        public int Kop { get; set; }
+        public override string ToString()
+        {
+            return $"{Rub} руб. {Kop} коп.";
+        }
+
+        public static Money operator +(Money m, Money n)
+        {
+            Money sum = new Money
+            {
+                Rub = m.Rub + n.Rub,
+                Kop = m.Kop + n.Kop
+            };
+            if (sum.Kop > 100)
+            {
+                sum.Rub++;
+                sum.Kop -= 100; 
+            }
+            return sum;
+        }
+        public static Money operator -(Money m, Money n)
+        {
+
+            Money dif = new Money
+            {
+                Rub = m.Rub - n.Rub,
+                Kop = m.Kop - n.Kop
+            };
+            if (dif.Kop < 0)
+            {
+                dif.Rub--;
+                dif.Kop += 100;
+            }
+            return dif;
+
+        }
+
+        public static Money operator *(Money m, int x)
+        {
+            Money rez = new Money
+            {
+                Kop = m.Kop * x,
+                Rub = m.Rub * x
+            };
+            if (rez.Kop > 100)
+            {
+                rez.Rub += (rez.Kop - (rez.Kop % 100)) / 100;
+                rez.Kop %= 100;
+            }
+            return rez;
+        }
+
+    }
+}
---
+++ hw3_2/Program.cs
@@ -0,0 +1,88 @@
+﻿//создать перегрузки: сложение 2х денег, вычитание, умножение на коэффициент
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace hw3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                WriteLine("Меню:\n" +
+                    "1. Сложение двух денежных сумм;\n" +
+                    "2. Вычитание одной денежной суммы из другой;\n" +
+                    "3. Умножение денежной суммы на коэффициент;\n" +
+                    "4. Выйти из программы.\n\n Какое действие хотите произвести:\n");
+                int pos = Convert.ToInt32(ReadLine());
+
+                switch (pos)
+                {
+                    case 1:
+                        {
+                            WriteLine("Сложение двух денежных сумм:\n");
+                            Write("Введите первую сумму:\nрублей: ");
+                            Money m = new Money();
+                            m.Rub = Convert.ToInt32(ReadLine());
+                            Write("копеек: ");
+                            m.Kop = Convert.ToInt32(ReadLine());
+                            Write("Введите вторую сумму:\nрублей: ");
+                            Money n = new Money();
+                            n.Rub = Convert.ToInt32(ReadLine());
+                            Write("копеек: ");
+                            n.Kop = Convert.ToInt32(ReadLine());
+                            WriteLine($"{m} + {n} = {m + n}");
+                            WriteLine();
+                        }
+                        break;
+
+                    case 2:                        
+                        {
+                            WriteLine("Вычитание одной денежной суммы из другой:\n");
+                            Write("Введите первую сумму:\nрублей: ");
+                            Money m = new Money();
+                            m.Rub = Convert.ToInt32(ReadLine());
+                            Write("копеек: ");
+                            m.Kop = Convert.ToInt32(ReadLine());
+                            Write("Введите вторую сумму:\nрублей: ");
+                            Money n = new Money();
+                            n.Rub = Convert.ToInt32(ReadLine());
+                            Write("копеек: ");
+                            n.Kop = Convert.ToInt32(ReadLine());
+                            WriteLine($"{m} - {n} = {m - n}");
+                            WriteLine();
+                        }
+                        break;
+                    case 3:
+                        {
+                            WriteLine("Умножение денежной суммы на коэффициент:\n");
+                            Write("Введите сумму:\nрублей: ");
+                            Money m = new Money();
+                            m.Rub = Convert.ToInt32(ReadLine());
+                            Write("копеек: ");
+                            m.Kop = Convert.ToInt32(ReadLine());
+                            Write("Введите коэффициент: ");
+                            int n = Convert.ToInt32(ReadLine());
+                            WriteLine($"{m} * {n} = {m * n}");
+                            WriteLine();
+                        }
+                        break;
+                    case 4:
+                        return;
+
+                    default:
+                        WriteLine("Вы выбрали несуществующий пункт меню. Выберите снова.");
+                        continue;
+                }
+            }
+        }
+    }
+}
+
+
